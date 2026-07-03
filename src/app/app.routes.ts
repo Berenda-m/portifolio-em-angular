7e5e3b320045780e172cbcacc9c3a4acd6a26acc
Brenda Melo <brendasilvam7@gmail.com>
2026-07-03 15:41:30 -0300

    atualiza

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Contato } from './componentes/contato/contato';
+import { Header } from './componentes/header/header';
+import { Home } from './componentes/home/home';
+import { Footer } from './componentes/footer/footer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: 'contato', component: Contato },
+    { path: 'home', component: Home },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
+
+
+
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './componentes/header/header';
+import { Header } from "./componentes/header/header";
 import { Home } from './componentes/home/home';
 import { Footer } from './componentes/footer/footer';
+import { Contato } from './componentes/contato/contato';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Home, Footer],
+  standalone: true,
+  imports: [RouterOutlet, Header, Home, Footer, Contato],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
