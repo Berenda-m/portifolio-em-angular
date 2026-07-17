b7a0a8579fdba0f716ce7252f60ad9bbc7336898
Brenda <brendasilvam7@gmail.com>
2026-07-17 17:12:51 -0300

    cria-input

--- src/app/componentes/projetos/projetos.ts
+++ src/app/componentes/projetos/projetos.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-projetos',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './projetos.html',
   styleUrl: './projetos.css',
 })
-export class Projetos {}
+export class Projetos {
+
+  texto: string = '';
+}
