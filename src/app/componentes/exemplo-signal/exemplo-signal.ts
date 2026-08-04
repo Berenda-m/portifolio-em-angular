03f7d5e719b19c54a55a28780948f4f2eacc22b2
brenda <brendasilvam7@gmail.com>
2026-08-04 17:21:49 -0300

    exercicio-02

--- src/app/componentes/exemplo-signal/exemplo-signal.ts
+++ src/app/componentes/exemplo-signal/exemplo-signal.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Usuario } from '../../interfaces/usuario';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-exemplo-signal',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './exemplo-signal.html',
   styleUrl: './exemplo-signal.css',
 })
-export class ExemploSignal {}
+export class ExemploSignal {
+
+   protected usuario= signal <Usuario> ({
+    id: 1,
+    nome: 'Brenda',
+    idade: 28
+  });
+}
