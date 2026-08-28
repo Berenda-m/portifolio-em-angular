d78ee38d7d3a0751e201c01744149c873ade1d39
brenda <brendasilvam7@gmail.com>
2026-08-28 17:17:58 -0300

    finaliza-exercicio

--- src/app/componentes/post-usuario/post-usuario-service.ts
+++ src/app/componentes/post-usuario/post-usuario-service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { PostUsuarioInterface } from '../../interfaces/post-usuario-interface';
 
 @Injectable({
   providedIn: 'root',
@@ -9,5 +10,10 @@ export class PostUsuarioService {
 private readonly http= inject(HttpClient)
 
 private urlApi= 'https://jsonplaceholder.typicode.com/users'
+
+
+cadastrarUsuarioPost(postUsuario: PostUsuarioInterface){
+  return this.http.post<PostUsuarioInterface>(this.urlApi, postUsuario);
+};
 
 }
--- src/app/componentes/post-usuario/post-usuario.ts
+++ src/app/componentes/post-usuario/post-usuario.ts
@@ -24,6 +24,28 @@ export class PostUsuario {
 
   protected postUsuarioForm= form(this.postUsuarioModel);
 
+  protected cadastrarUsuario(event: SubmitEvent){
+    event.preventDefault();
+    const post= this.postUsuarioModel();
 
-  
+  this.postUsuarioService.cadastrarUsuarioPost(post).subscribe({
+    next: () =>{
+      alert('Post cadastrado')
+
+      this.postUsuarioModel.set({
+        name: '',
+        username:'',
+        email:'',
+        phone: null
+      });
+      this.postUsuarioForm().reset();
+    },
+
+    error: ()=>{
+      alert('Algo deu errado')
+    }
+  })
+
+  }
+
 }
